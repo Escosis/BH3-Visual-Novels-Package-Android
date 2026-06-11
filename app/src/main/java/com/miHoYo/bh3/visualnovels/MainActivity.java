ea150c8d35fb7fde82d90d5307cead8d96aa5eb1
Escosis <1906317507@qq.com>
2026-06-11 11:48:33 +0800

    Initial commit

---
+++ app/src/main/java/com/miHoYo/bh3/visualnovels/MainActivity.java
@@ -0,0 +1,55 @@
+/**
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+*/
+
+package com.miHoYo.bh3.visualnovels;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+            moveTaskToBack(true);
+        }
+
+        Window window = this.getWindow();
+
+        // 设置全屏标志，并自动隐藏状态栏
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY          // 沉浸模式（用户滑动可呼出）
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN              // 隐藏状态栏
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION         // 隐藏导航栏
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
+
+        // Set by <content src="index.html" /> in config.xml
+        loadUrl(launchUrl);
+    }
+}
